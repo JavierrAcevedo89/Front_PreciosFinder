2963cecab6fa1fbceefcd455c4bed53f340b2d58
SoryQ14 <squibtanarosas64@gmail.com>
2024-10-09 09:57:52 -0600

    horror

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: 'src\app\app.component.html',
+ // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'precioFinder';
