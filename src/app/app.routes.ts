d472875af20e94ea1a9ad043bbd2325d183987da
Javier Acevedo <fah@dnu.com.mx>
2024-10-09 16:09:25 -0600

    Cambio para versión inicial Se mueven a /assets las imágenes y se referencía el componente "buscador"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { BuscadorComponent } from './screen/buscador/buscador.component';
 
 export const routes: Routes = [];
--- src/app/screen/buscador/buscador.component.ts
+++ src/app/screen/buscador/buscador.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   selector: 'app-buscador',
   standalone: true,
   imports: [],
-  templateUrl: './buscador.component.css',
+  templateUrl: './buscador.component.html',
   styleUrls: ['./buscador.component.css']
 })
 export class BuscadorComponent {
